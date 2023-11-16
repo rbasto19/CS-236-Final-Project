64603ad8e5aa013bf07892a11ccfdae37f62c49a
rbasto19 <65095336+rbasto19@users.noreply.github.com>
2023-11-15 16:11:28 -0800

    Add files via upload

---
+++ GeoDiff-pretrain/debugging_tests.py
@@ -0,0 +1,8 @@
+import pickle
+from rdkit.Chem.Draw import IPythonConsole
+import torch
+# with open('/Users/rbasto/Downloads/GeoDiff-pretrain/data/GEOM/QM9/val_data_5k.pkl', 'rb') as f:
+#     data = pickle.load(f)
+a = [1,2,3,4,5]
+print(a[:4])
+print(a[4:])
